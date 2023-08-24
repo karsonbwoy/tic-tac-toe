2acf2eaae82fbf388a578878813428d9f62cfa45
Sebastian <skarapyta@gmail.com>
2023-08-24 11:27:54 +0200

    Add calculating winner and  game status

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function Board() {
 
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null))
- 
+  
   function handleClick(i) {
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
       return;
     }
     const nextSquares = squares.slice();
@@ -25,8 +25,21 @@ function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status = "Winner is: " + winner;
+  } else if(!squares.includes(null)){
+    status = "Draw."
+  } else {
+    status = "Next move is: " + (xIsNext ? 'X' : '0')
+  }
+  
+
   return (
     <>
+      <div className='status'>{status}</div>
       <div className='board-row'>
         <Square value = {squares[0]} onSquareClick = {() => handleClick(0)}/>
         <Square value = {squares[1]} onSquareClick = {() => handleClick(1)}/>
@@ -46,6 +59,26 @@ function Board() {
   )
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for(let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 export default function App(){
  return (
